4fd1d15f1cec191e6747bd1c13477694d35c7e8e
wanglei <wanglei09@corp.netease.com>
2023-03-09 11:41:11 +0800

    remove the files include private api

--- YYText/YYText.h
+++ YYText/YYText.h
@@ -15,7 +15,6 @@
 FOUNDATION_EXPORT double YYTextVersionNumber;
 FOUNDATION_EXPORT const unsigned char YYTextVersionString[];
 #import <YYText/YYLabel.h>
-#import <YYText/YYTextView.h>
 #import <YYText/YYTextAttribute.h>
 #import <YYText/YYTextArchiver.h>
 #import <YYText/YYTextParser.h>
@@ -25,14 +24,12 @@ FOUNDATION_EXPORT const unsigned char YYTextVersionString[];
 #import <YYText/YYTextLine.h>
 #import <YYText/YYTextInput.h>
 #import <YYText/YYTextDebugOption.h>
-#import <YYText/YYTextKeyboardManager.h>
 #import <YYText/YYTextUtilities.h>
 #import <YYText/NSAttributedString+YYText.h>
 #import <YYText/NSParagraphStyle+YYText.h>
 #import <YYText/UIPasteboard+YYText.h>
 #else
 #import "YYLabel.h"
-#import "YYTextView.h"
 #import "YYTextAttribute.h"
 #import "YYTextArchiver.h"
 #import "YYTextParser.h"
@@ -42,7 +39,6 @@ FOUNDATION_EXPORT const unsigned char YYTextVersionString[];
 #import "YYTextLine.h"
 #import "YYTextInput.h"
 #import "YYTextDebugOption.h"
-#import "YYTextKeyboardManager.h"
 #import "YYTextUtilities.h"
 #import "NSAttributedString+YYText.h"
 #import "NSParagraphStyle+YYText.h"
